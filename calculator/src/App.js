67bbeba018564ebe64249798765be9413ee99e39
Jú Garcia <spacejugarcia@gmail.com>
2024-01-16 09:58:36 -0300

    update

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Calculadora!<h1/>
-      </header>
+       <Calculator/>
     </div>
   );
 }
